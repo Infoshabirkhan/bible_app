6379ae459e4e8f6ed6f45cadba88897b40b939ff
fahadshabeer <fadiktk005@gmail.com>
2023-03-31 00:48:28 +0500

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,22 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
-
 import 'Controllers/Cubits/bottom_navigaiton_cubit.dart';
 import 'Controllers/Cubits/chapter_cubit/chapter_cubit.dart';
 import 'Views/authentication_screen/login_screen/login_screen.dart';
 import 'Views/authentication_screen/signup_screen/sign_up_screen.dart';
 import 'Views/bottom_navigation_screens/bottom_navigation_screen.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'firebase_options.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-await   MobileAds.instance.initialize();
+  await MobileAds.instance.initialize();
 
-  await  Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -29,25 +33,28 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (context)=> BottomNavigationCubit(0)),
-        BlocProvider(create: (context)=> AuthenticationCubit()),
-        BlocProvider(create: (context)=> BibleTaskCubit()),
-        BlocProvider(create: (context)=> BibleBooksCubit()),
-        BlocProvider(create: (context)=> ChapterCubit()),
+        BlocProvider(create: (context) => BottomNavigationCubit(0)),
+        BlocProvider(create: (context) => AuthenticationCubit()),
+        BlocProvider(create: (context) => BibleTaskCubit()),
+        BlocProvider(create: (context) => BibleBooksCubit()),
+        BlocProvider(create: (context) => ChapterCubit()),
       ],
       child: ScreenUtilInit(
 
-        builder: (BuildContext context, Widget? child) => MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Flutter Demo',
-          theme: ThemeData(
+        builder: (BuildContext context, Widget? child) =>
+            MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Flutter Demo',
+              theme: ThemeData(
 
-            useMaterial3: true,
-            brightness: Brightness.dark,
-            colorSchemeSeed: Colors.blue,
-          ),
-          home: FirebaseAuth.instance.currentUser !=null ? BottomNavigationScreen(): LoginScreen(),
-        ),
+                useMaterial3: true,
+                brightness: Brightness.dark,
+                colorSchemeSeed: Colors.blue,
+              ),
+              home: FirebaseAuth.instance.currentUser != null
+                  ? BottomNavigationScreen()
+                  : LoginScreen(),
+            ),
       ),
     );
   }
