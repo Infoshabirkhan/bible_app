57d46e10bb7fd54a37ab278ad3c3ed69bc637d0e
apple <https://github.com/JMM-Technologies-Ltd/UnityPath>
2023-04-12 16:28:09 +0500

    AD ids updates

--- lib/Models/advertisement_id.dart
+++ lib/Models/advertisement_id.dart
@@ -1,9 +1,9 @@
 class AdvertisementID{
 
-  static var  bannerAndroidId= 'ca-app-pub-3940256099942544/6300978111'; // test Id
- // static var  bannerAndroidId= 'ca-app-pub-3984446731921802/9593023561'; // actual Id
-  static var  bannerIosId= 'ca-app-pub-3940256099942544/2934735716'; // test Id
-  //static var  bannerIosId= 'ca-app-pub-3984446731921802/3542620535'; // actual Id
+  // static var  bannerAndroidId= 'ca-app-pub-3940256099942544/6300978111'; // test Id
+ static var  bannerAndroidId= 'ca-app-pub-3984446731921802/9593023561'; // actual Id
+  // static var  bannerIosId= 'ca-app-pub-3940256099942544/2934735716'; // test Id
+  static var  bannerIosId= 'ca-app-pub-3984446731921802/3542620535'; // actual Id
 
 
 
@@ -11,10 +11,10 @@ class AdvertisementID{
 
 
 
-  static var  interstitialAndroidId = 'ca-app-pub-3940256099942544/1033173712'; // test Id
-  // static var  interstitialAndroidId = 'ca-app-pub-3984446731921802/9533313818'; // actual Id
-  static var  interstitialIosId = 'ca-app-pub-3940256099942544/4411468910'; // test Id
-  // static var  interstitialIosId = 'ca-app-pub-3984446731921802/3123818136'; // actual Id
+  // static var  interstitialAndroidId = 'ca-app-pub-3940256099942544/1033173712'; // test Id
+  static var  interstitialAndroidId = 'ca-app-pub-3984446731921802/9533313818'; // actual Id
+  // static var  interstitialIosId = 'ca-app-pub-3940256099942544/4411468910'; // test Id
+  static var  interstitialIosId = 'ca-app-pub-3984446731921802/3123818136'; // actual Id
 
 
 
