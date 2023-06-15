87637cb20a412368acf3155f1f3d2f9510bfa835
infoshabbirkhan <infoshabbirkhan@gmail.com>
2023-06-15 10:46:13 +0500

    published on stores

--- lib/Models/advertisement_id.dart
+++ lib/Models/advertisement_id.dart
@@ -5,8 +5,8 @@ class AdvertisementID{
   //==================================== Test  ids ====================//
 
   /// Android Test ===============
- static var  bannerAndroidId= 'ca-app-pub-3940256099942544/6300978111'; // test Id
- static var  interstitialAndroidId = 'ca-app-pub-3940256099942544/1033173712'; // test Id
+  static var  bannerAndroidId= 'ca-app-pub-3940256099942544/6300978111'; // test Id
+  static var  interstitialAndroidId = 'ca-app-pub-3940256099942544/1033173712'; // test Id
 
   /// IOS Test===================
 
