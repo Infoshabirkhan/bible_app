b5c769a210ac3edeff7dad744c7d2954a0a1d35a
infoshabbirkhan <infoshabbirkhan@gmail.com>
2023-06-17 09:58:43 -0700

    uploaded

--- lib/Models/advertisement_id.dart
+++ lib/Models/advertisement_id.dart
@@ -5,8 +5,8 @@ class AdvertisementID{
   //==================================== Test  ids ====================//
 
   /// Android Test ===============
-  static var  bannerAndroidId= 'ca-app-pub-3940256099942544/6300978111'; // test Id
-  static var  interstitialAndroidId = 'ca-app-pub-3940256099942544/1033173712'; // test Id
+  // static var  bannerAndroidId= 'ca-app-pub-3940256099942544/6300978111'; // test Id
+  // static var  interstitialAndroidId = 'ca-app-pub-3940256099942544/1033173712'; // test Id
 
   /// IOS Test===================
 
@@ -22,8 +22,8 @@ class AdvertisementID{
 
 
   /// Android Actual ==================
-  //  static var  bannerAndroidId= 'ca-app-pub-3984446731921802/9593023561'; // actual Id
-  //  static var  interstitialAndroidId = 'ca-app-pub-3984446731921802/9533313818'; // actual Id
+    static var  bannerAndroidId= 'ca-app-pub-3984446731921802/9593023561'; // actual Id
+    static var  interstitialAndroidId = 'ca-app-pub-3984446731921802/9533313818'; // actual Id
 
 
 
